5de282dc5ecbd63a083f6b558cd178e477368d14
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-08-27 11:08:11 +0600

    Add files via upload

---
+++ maximum_sub_array.cpp
@@ -0,0 +1,52 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+// #include <string>
+// #include <sstream>
+// #include<set>
+// #include<iomanip>
+// #include<cstring>
+// #include<cctype> //islower () issuper () for check the first char
+// #include<algorithm>
+// #include<cmath>
+#define sortarray sort(arr,arr+x);
+// #define rev reverse(s1.begin(), s1.end());
+// #define uppertolowers1 transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+// #define uppertolowers2 transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+// #define descsort sort(arr, arr + n, greater<int>());
+#define sp " "
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define maxelementofvector *max_element(digit.begin(),digit.end());
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+const long long N=1e6+15;
+void solve ()
+{
+    ll n,k;cin>>n>>k;
+    vector<int>vv(n);vector<long long>pre(n+1,0);
+    for (auto &x:vv)cin>>x;
+    for (int i=1;i<n+1;i++)
+    {
+        pre[i]+=pre[i-1]+vv[i-1];
+    }
+    ll mx=pre[k];
+    for (int i=k+1;i<n+1;i++)
+    {
+        ll temp=pre[i]-pre[i-k];
+        mx=max(mx,temp);
+    }
+    cout<<mx<<endl;
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
